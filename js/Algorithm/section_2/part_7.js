40c9341add9ea18b62eaed33b98b659ac2f94e08
JunSeo <oponize@naver.com>
2021-05-23 21:58:10 +0900

    lear Algorithm

--- js/Algorithm/section_2/part_7.js
+++ js/Algorithm/section_2/part_7.js
@@ -5,21 +5,25 @@
 function solution(arr){  
     let answer = 0;
     let n = arr.length;
+    // 초기화시켜준다
     let dx = [-1, 0, 1, 0];
     let dy = [0, 1, 0, -1];
 
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
+            // 큰 값이 있는지 위한 확인 변수
             let flag = 1;
             for (let k = 0; k < 4; k++) {
                 let nx = i + dx[k];
                 let ny = j + dy[k];
 
+                // 4방위 중에 배열 범위를 벗어나지 않게
                 if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[i][j] < arr[nx][ny]) {
                     flag = 0;
                     break;
                 }
             }
+
             if(flag) {
                 answer++;
             }
