00e3e831e7f18165e4f2a4e71fe91d0d94ea0a12
JunSeo <oponize@naver.com>
2021-06-02 20:28:38 +0900

    learn array

--- js/datastruct/array/tictactok.js
+++ js/datastruct/array/tictactok.js
@@ -1,30 +1,113 @@
-// 3x3 
-// 행과 열 검사
-// 대각선 검사
-// 세개 연달아 같은 문자인 경우 true 반환
-
-// 이전 값을 저장하고 같다면 트루, 아니면 펄스
+// 앞뒤 비교해서 같다면 count++ 
+// 카운트가 2가 있다면 true
 
 
-function ticTacTok (array, who) {
+function ticTacTok (array) {
     let leng = array.length;
-    let temp = [];
+    let count;
     
     // 행 검사
     for (let i = 0; i < leng; i++) {
-        for (let j = 0; j < leng; j++) {
-            if ((array[i][j] !== who) || (array[j][i] !== who)) {
-                return false;
+        count = 0;
+        for (let j = 0; j < leng - 1; j++) {
+            if ((array[i][j] === array[i][j + 1])) {
+                count++;
+                if (count === 2) {
+                    console.log(array[i][j + 1]);
+                    return true;
+                }
             }
+        }
+    }
+
+    // 열 검사
+    for (let i = 0; i < leng; i++) {
+        count = 0;
+        for (let j = 0; j < leng - 1; j++) {
+            if ((array[j][i] === array[j + 1][i])) {
+                count++;
+                if (count === 2) {
+                    console.log(array[j + 1][i]);
+                    return true;
+                }
+            }
+        }
+    }
+
+    count = 0;
+
+    for (let i = 0; i < leng - 1; i++) {
+        if (array[i][i] === array[i + 1][i + 1]) {
+            count++;
+            if (count === 2) {
+                console.log(array[i + 1][i + 1]);
+                return true;
+            }
+        }
+    }
+
+    count = 0;
+
+    for (let i = 0; i < leng - 1; i++) {
+        if (array[i][leng - 1 - i] === array[i + 1][leng - 1 - (i + 1)]) {
+            count++;
+            if (count === 2) {
+                console.log(array[i + 1][leng - 1 - (i + 1)]);
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+let arr = [
+    ["O", "X", "O"],
+    ["X", "O", "X"],
+    ["O", "X", "X"]
+];
+
+console.log(ticTacTok(arr));
+
+/*
+
+function checkRow ( rowArr, letter ) {
+    for ( var i=0; i < 3; i++) {
+        if (rowArr[i]!=letter) {
+            return false;
         }
     }
     return true;
 }
 
-let arr = [
-    ["O", "X", "-"],
-    ["O", "X", "O"],
-    ["X", "X", "O"]
-];
+function checkColumn ( gameBoardMatrix, columnIndex, letter ) {
+    for ( var i=0; i < 3; i++) {
+        if (gameBoardMatrix[i][columnIndex]!=letter) {
+            return false;
+        }
+    }
+    return true;
+}
 
-console.log(ticTacTok(arr, "X"));
+function ticTacToeWinner ( gameBoardMatrix, letter) {
+
+    // Check rows
+    var rowWin = checkRow(gameBoardMatrix[0], letter)
+        || checkRow(gameBoardMatrix[1], letter)
+        || checkRow(gameBoardMatrix[2], letter);
+
+    var colWin = checkColumn(gameBoardMatrix, 0, letter)
+        || checkColumn(gameBoardMatrix, 1, letter)
+        || checkColumn(gameBoardMatrix, 2, letter);
+
+    var diagonalWinLeftToRight = (gameBoardMatrix[0][0]==letter && gameBoardMatrix[1][1]==letter && gameBoardMatrix[2][2]==letter);
+    var diagonalWinRightToLeft = (gameBoardMatrix[0][2]==letter && gameBoardMatrix[1][1]==letter && gameBoardMatrix[2][0]==letter);
+
+    return rowWin || colWin || diagonalWinLeftToRight || diagonalWinRightToLeft;
+}
+
+var board = [['O','-','X'],['-','O','-'],['-','X','O']];
+ticTacToeWinner(board, 'X'); // false
+ticTacToeWinner(board, 'O'); // true
+
+*/
