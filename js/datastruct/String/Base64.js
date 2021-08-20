075169f83a2a242529602a2fd5fa69df751cf50a
JunSeo <oponize@naver.com>
2021-08-20 22:29:09 +0900

    fix

--- js/datastruct/String/Base64.js
+++ js/datastruct/String/Base64.js
@@ -1,5 +1,1 @@
 const str = "React is Very Awesome FrameWork..!";
-
-const n = window.btoa(str);
-
-console.log(n);
--- js/ts_learn/test_compile/index.ts
+++
@@ -1,6 +0,0 @@
-const add = (a: number, b: number) => {
-  return a + b;
-};
-
-add(10, 20);
-// tsc 명령어로 js파일로 컴파일
