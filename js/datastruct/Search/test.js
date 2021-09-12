b61206bdabaa8c2b4d454a121a6dedc454169887
JunSeo <oponize@naver.com>
2021-09-12 13:33:15 +0900

    learn Vanila js

---
+++ js/datastruct/Search/test.js
@@ -0,0 +1,13 @@
+// let max = 0;
+const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+  if (max < arr[i]) {
+    max = arr[i];
+  }
+}
+
+// 자바스크립트 내장 객체 사용
+const max = Math.floor(Math.max(...arr));
+
+console.log(max);
--- js/datastruct/recurstion/test.js
+++ js/datastruct/recurstion/test.js
@@ -1,26 +1,43 @@
-let list = {
-    value: 1,
-    next: {
-        value: 5,
-        next: {
-            value: 3,
-            next: {
-                value: 4,
-                next: null
-            }
-        }
+// let list = {
+//     value: 1,
+//     next: {
+//         value: 5,
+//         next: {
+//             value: 3,
+//             next: {
+//                 value: 4,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+
+// let subList = list.next.next;
+// list.next.next = null;
+
+// function printList (li) {
+//     console.log(li.value);
+//     if (li.next) {
+//         printList(li.next);
+//     }
+// }
+
+// console.log(printList(subList));
+
+function fibo(n) {
+  if (n < 1) {
+    return n;
+  }
+  const arr = [0, 1];
+
+  const closure = (n) => {
+    if (arr[n] !== undefined) {
+      return arr[n];
+    } else {
+      return (arr[n] = closure(n - 1) + closure(n - 2));
     }
+  };
+  return closure(n);
 }
 
-let subList = list.next.next;
-list.next.next = null;
-
-
-function printList (li) {
-    console.log(li.value);
-    if (li.next) {
-        printList(li.next);
-    }
-}
-
-console.log(printList(subList));
+console.log(fibo(8));
