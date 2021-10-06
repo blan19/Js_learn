b7704b3ea93854dfdb871f8f8cef503a7eaa86ed
JunSeo <oponize@naver.com>
2021-10-06 13:18:46 +0900

    learn algorithm

--- js/datastruct/Search/QuickSort.js
+++ js/datastruct/Search/QuickSort.js
@@ -1,49 +1,71 @@
-// 퀵정렬
-// 왼쪽 수는 기준보다 작으면 다음으로 넘어가고, 크면 가만히 있는다. 
-// 오른쪽 수는 기준보다 크면 다음으로 넘어가고, 작으면 가만히 있는다. 
-// 이렇게 넘어가다가 왼쪽은 기준보다 크고, 오른쪽은 기준보다 작으면 서로 바꿔준다.
+let array = [8, 1, 3, 5, 6, 2, 0, 10, 9, 7, 11, 21, 22];
 
-let array = [1, 3, 5, 6, 2, 0, 8, 9, 7, 4];
+// function QuickSort(arr, left, right) {
+//   let idx;
 
-function QuickSort (arr, left, right) {
-    let idx;
+//   if (arr.length > 1) {
+//     idx = partition(arr, left, right);
 
-    if (arr.length > 1) {
-        idx = partition(arr, left, right);
+//     if (left < idx - 1) {
+//       QuickSort(arr, left, idx - 1);
+//     }
 
-        if (left < idx - 1) {
-            QuickSort(arr, left, idx - 1);
-        }
+//     if (idx < right) {
+//       QuickSort(arr, idx, right);
+//     }
+//   }
 
-        if (idx < right) {
-            QuickSort(arr, idx, right);
-        }
+//   return arr;
+// }
+
+// function partition(arr, letf, right) {
+//   let pivot = arr[Math.floor((letf + right) / 2)];
+
+//   while (letf <= right) {
+//     while (arr[letf] < pivot) {
+//       letf++;
+//     }
+
+//     while (arr[right] > pivot) {
+//       right--;
+//     }
+
+//     if (letf <= right) {
+//       [arr[letf], arr[right]] = [arr[right], arr[letf]];
+//       letf++;
+//       right--;
+//     }
+//   }
+//   return letf;
+// }
+
+// console.log(QuickSort(array, 0, array.length - 1));
+
+const partition = (arr, low, high, pivotPoint) => {
+  let i, j;
+  const pivotItem = arr[low];
+  j = low;
+
+  for (i = low + 1; i <= high; i++) {
+    if (arr[i] < pivotItem) {
+      j++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
+  }
 
-    return arr;
-}
+  pivotPoint = j;
+  [arr[low], arr[pivotPoint]] = [arr[pivotPoint], arr[low]];
+  return pivotPoint;
+};
 
-function partition (arr, letf, right) {
-    let pivot = arr[Math.floor((letf + right) / 2)];
+const quickSort = (arr, low, high) => {
+  let pivotPoint;
+  if (high > low) {
+    pivotPoint = partition(arr, low, high, pivotPoint);
+    quickSort(arr, low, pivotPoint - 1);
+    quickSort(arr, pivotPoint + 1, high);
+  }
+  return arr;
+};
 
-    while (letf <= right) {
-        while (arr[letf] < pivot) {
-            letf++;
-        }
-
-        while (arr[right] > pivot) {
-            right--;
-        }
-
-        if (letf <= right) {
-            let temp = arr[letf];
-            arr[letf] = arr[right];
-            arr[right] = temp;
-            letf++;
-            right--;
-        }
-    }
-    return letf;
-}
-
-console.log(QuickSort(array, 0, array.length - 1));
+console.log(quickSort(array, 0, array.length - 1));
