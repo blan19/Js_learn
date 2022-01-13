c4dade37fef8b8d25243a57c787fe622a77d3bc5
JunSeo <oponize@naver.com>
2022-01-13 21:41:52 +0900

    learn al

---
+++ js/Algorithm/ct/week_1/test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int v[10];
+
+int main() {
+  unordered_map<string, int> umap;
+  umap.insert({'test1', 1});
+  cout << umap.first << "\n";
+  return 0;
+}
