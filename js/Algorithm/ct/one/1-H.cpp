f82bcff25758d348aae24da026dd9dd1608b9315
JunSeo <oponize@naver.com>
2022-02-02 12:51:01 +0900

    learn al

--- js/Algorithm/ct/one/1-H.cpp
+++ js/Algorithm/ct/one/1-H.cpp
@@ -20,38 +20,20 @@ int main() {
   return 0;
 }
 
-/**
- * @file 1-H.cpp
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2022-01-23
- * 
- * @copyright Copyright (c) 2022
- * 
- */
-
-/**
- * @brief 
- * 
- * 이중 for
- * 
- */
-
-#include<bits/stdc++.h> 
-using namespace std;  
-typedef long long ll;  
-int n, k, temp, psum[100001], ret = -1000000; 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);cout.tie(NULL);
-	cin >> n >> k; 
-	for(int i = 1; i <= n; i++){
-		cin >> temp; psum[i] = psum[i - 1] + temp; 
-	} 
-	for(int i = k; i <= n; i++){
-		ret = max(ret, psum[i] - psum[i - k]);
-	}
-	cout << ret << "\n";
-    return 0;
-}
+// #include<bits/stdc++.h> 
+// using namespace std;  
+// typedef long long ll;  
+// int n, k, temp, psum[100001], ret = -1000000; 
+// int main(){
+// 	ios_base::sync_with_stdio(false);
+// 	cin.tie(NULL);cout.tie(NULL);
+// 	cin >> n >> k; 
+// 	for(int i = 1; i <= n; i++){
+// 		cin >> temp; psum[i] = psum[i - 1] + temp; 
+// 	} 
+// 	for(int i = k; i <= n; i++){
+// 		ret = max(ret, psum[i] - psum[i - k]);
+// 	}
+// 	cout << ret << "\n";
+//     return 0;
+// }
