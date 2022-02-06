d42d51f90e395b4b3d06167cf38ffdc0a02b5783
JunSeo <oponize@naver.com>
2022-02-06 16:20:29 +0900

    learn al

--- js/Algorithm/ct/one/1-I.cpp
+++ js/Algorithm/ct/one/1-I.cpp
@@ -1,12 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int n, m;
 string s; 
 map<string, int> _map;
 map<int, string> _map2;
 int main(){  
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m; 
 	for(int i = 0; i < n; i++){
 		cin >> s; 
@@ -15,10 +14,14 @@ int main(){
 	}
 	for(int i = 0; i < m; i++){
 		cin >> s; 
-		if(atoi(s.c_str()) == 0){
+		if(stoi(s) == 0){
 			cout << _map[s] << "\n";
-		}else{
+		} else {
 			cout << _map2[atoi(s.c_str())] << "\n"; 
 		}
 	} 
 }
+
+// atoi : chat * -> int; (int 부분만 빼서 형변환)
+// c.str : string -> char *
+//
---
+++ js/Algorithm/ct/one/1-J.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int test, input;
+string s_1, s_2;
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  cin >> test;
+  for(int i = 0; i < test; i++) {
+    cout << "\n";
+    cin >> input;
+    for(int j = 0; j < input; j++) {
+      cin >> s_1 >> s_2;
+    }
+  }
+  return 0;
+}
+
+// 조합(combination)
