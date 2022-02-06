f1b6c475999b3d5513dbb5e6fb8035dc7680dc9a
JunSeo <oponize@naver.com>
2022-02-06 17:55:18 +0900

    learn al

--- js/Algorithm/ct/one/1-J.cpp
+++ js/Algorithm/ct/one/1-J.cpp
@@ -1,18 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-int test, input;
-string s_1, s_2;
-int main() {
-  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-  cin >> test;
-  for(int i = 0; i < test; i++) {
-    cout << "\n";
-    cin >> input;
-    for(int j = 0; j < input; j++) {
-      cin >> s_1 >> s_2;
+int t, n;
+string a, b;
+int main(){
+    cin >> t;
+    while(t--){
+        map<string, int> _map;
+        cin >> n;
+         for(int i = 0; i < n; i++){
+            cin >> a >> b;
+            _map[b]++;
+        }
+        long long ret = 1;
+        for(auto c : _map){
+            ret *= ((long long)c.second + 1);
+        }
+        ret--;
+        cout << ret << "\n";
     }
-  }
-  return 0;
+    return 0;
 }
 
-// 조합(combination)
+// #include <bits/stdc++.h>
+// using namespace std;
+// map<string, string> omap;
+// string ct, cl;
+// int n;
+// int main() {
+//   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   cin >> n;
+//   for(int i = 0; i < n; i++) {
+//     cin >> cl >> ct;
+//     auto search = omap.find(ct);
+//     if(search != omap.end()) {
+
+//     }
+//   }
+  
+//   return 0;
+// }
+
+/**
+ * @brief 
+ * n
+ * for <= n
+ * cloth input
+ * 중복검사 -> 맵
+ * 조합
+ */
