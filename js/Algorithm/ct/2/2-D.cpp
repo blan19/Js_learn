69a114d5d5fd52221d0ffdda8c198caa46fb4ddc
Junseo <oponize@naver.com>
2022-02-27 10:26:07 +0900

    learn al

--- js/Algorithm/ct/2/2-D.cpp
+++ js/Algorithm/ct/2/2-D.cpp
@@ -1,47 +1,43 @@
-#include<bits/stdc++.h>
-using namespace std;   
-int b[101][101], e[101][101], visited[101][101], n, temp, ret = 1;
+#include <bits/stdc++.h>
+using namespace std;
+const int max_l = 101;
+int m,n,k,visited[max_l][max_l], e[max_l][max_l];
+vector<int> _ret;
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {0, 1, 0, -1};
-void dfs(int y, int x){
-    visited[y][x] = 1; 
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i]; 
-        int nx = x + dx[i]; 
-        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue; 
-        if(!visited[ny][nx] && e[ny][nx] == 0) dfs(ny, nx);
+int dfs(int y, int x) {
+    int temp = 1;
+    visited[y][x] = 1;
+    for(int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || ny >= m || nx < 0 || nx >= n || e[ny][nx]) continue;
+        if(visited[ny][nx] == 1) continue;
+        temp += dfs(ny, nx);
     }
-    return;
-} 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);   
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> b[i][j];
-        }
-    }
-    for(int d = 1; d < 101; d++){
-        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0); 
-        temp = 0; 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(e[i][j] == 1) continue;
-                if(b[i][j] <= d) e[i][j] = 1;
+    return temp;
+}
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> m >> n >> k;
+    while(k--) {
+        int bx, by, tx, ty;
+        cin >> bx >> by >> tx >> ty;
+        for(int i = bx; i < tx; i++) {
+            for(int j = by; j < ty; j++) {
+                e[j][i] = 1;
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(e[i][j] == 0 && !visited[i][j]) {
-                    dfs(i, j);
-                    temp++;
-                }
+    }
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(e[i][j] == 0 && !visited[i][j]) {
+              _ret.push_back(dfs(i, j));
             }
         }
-        ret = max(ret, temp);
     }
-    cout << ret << '\n';
-    return 0; 
+    cout << _ret.size() << "\n";
+    sort(_ret.begin(), _ret.end());
+    for(int iter : _ret) cout << iter << "\n"; 
+    return 0;
 }
