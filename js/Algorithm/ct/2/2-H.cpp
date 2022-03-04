3011ad7910c76b14ea53e3dc34cc91856d2e0fb0
JunSeo <oponize@naver.com>
2022-03-04 15:45:40 +0900

    learn al

--- js/Algorithm/ct/2/2-H.cpp
+++ js/Algorithm/ct/2/2-H.cpp
@@ -11,26 +11,29 @@ bool isVowel(int idx){
 	return (idx == 0 || idx == 4 || idx == 8 || idx == 14 || idx == 20);
 }
 int main () { 
-	while(true){
-		cin >> s; 
-		if(s == "end")break;
-		memset(cnt, 0, sizeof(cnt));
-		lcnt = vcnt = 0;
-		bool flag = 0;
-		int prev = -1;
-		for(int i = 0; i < s.size(); i++){
-			int idx = s[i] - 'a'; 
-			cnt[idx]++; 
-			if(isVowel(idx))lcnt++, vcnt = 0;
-			else vcnt++, lcnt = 0;
-			if(vcnt == 3 || lcnt == 3) flag = 1; 
-			if(i >= 1 && (s[i - 1] == s[i]) && (idx != 4 && idx != 14)){
+  while(true) {
+    cin >> s;
+    if(s == "end") break;
+    memset(cnt, 0, sizeof(cnt));
+    vcnt = lcnt = 0;
+    bool flag = 0;
+    for(int i = 0; i < s.size(); i++) {
+      int idx = s[i] - 'a';
+      if(isVowel(idx)) {
+        vcnt++;
+        lcnt = 0;
+      } else {
+        vcnt = 0;
+        lcnt++;
+      }
+      if(lcnt == 3 || vcnt == 3) flag = 1;
+      if(i >= 1 && (s[i - 1] == s[i]) && (idx != 4 && idx != 14)){
 				flag  = 1;
 			} 
-		}
-		if(!cnt[0] && !cnt[4] && !cnt[8] && !cnt[14] && !cnt[20])flag = 1; 
-		if(flag) cout << "<" << s << ">" << " is not acceptable.\n";
+    }
+    if(!cnt[0] && !cnt[4] && !cnt[8] && !cnt[14] && !cnt[20]) flag = 1;
+    if(flag) cout << "<" << s << ">" << " is not acceptable.\n";
 		else cout << "<" << s << ">" << " is acceptable.\n";
-	} 
+  }
 	return 0;
 }
--- js/Algorithm/ct/2/2-I.cpp
+++ js/Algorithm/ct/2/2-I.cpp
@@ -1,5 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int max_l = 10001;
+int n, ret[max_l];
+vector<string> v;
+string s;
+bool cmp(string a, string b){
+	if(a.size() == b.size()) return a < b;
+	return a.size() < b.size();
+}
 int main () {
+  cin >> n;
+  while(n--) {
+    cin >> s;
+    string temp = "";
+    for(int i = 0; i < s.size(); i++) {
+      int idx = s[i] - 48;
+      if(idx >= 0 && idx <= 9) {
+        temp += s[i];
+      } else {
+        if(temp.size()) {
+          while(true) {
+            if(temp.size() && temp.front() == '0') temp.erase(temp.begin());
+            else break;
+          }
+          if(temp.size() == 0) temp = '0';
+          v.push_back(temp);
+          temp = "";
+        }
+      }
+    }
+    if(temp.size()) {
+      while(true) {
+        if(temp.size() && temp.front() == '0') temp.erase(temp.begin());
+        else break;
+      }
+      if(temp.size() == 0) temp = '0';
+      v.push_back(temp);
+      temp = "";
+    }
+  }
+  sort(v.begin(), v.end(), cmp);
+  for(auto iter : v) cout << iter << "\n";
   return 0;
 }
