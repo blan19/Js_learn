4bc542ba0f6a5e602bf8dc30497aa95777a6d47a
JunSeo <oponize@naver.com>
2022-03-05 13:00:16 +0900

    learn al

--- learn_something/Algorithm/ct/2/2-K.cpp
+++ learn_something/Algorithm/ct/2/2-K.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int t, n;
-int factorial(ll n) {
-  if(n <= 1) {
-    return 1;
-  }
-  return n * factorial(n-1);
-}
+int t, n, retA, retB;
 int main () {
-  // while(t--) {
-  //   cin >> n;
-  // }
-  cout << factorial(10) << "\n";
+  cin >> t;
+  while(t--) {
+    cin >> n;
+    retA = 0; 
+    retB = 0;
+    for(int i = 2; i <= n; i*=2) {
+      retA += n / i;
+    }
+    for(int i = 5; i <= n; i*=5) {
+      retB += n / i;
+    }
+    cout << min(retA, retB) << "\n";
+  }
   return 0;
 }
